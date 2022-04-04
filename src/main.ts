4db2de7686903c83418396ed1aeb49fcb3eab498
AlexeevSasha <work.alexeevs@gmail.com>
2022-04-04 16:22:00 +0300

    app binding container modules

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,26 @@ import {App} from "./app";
 import {LoggerService} from "./logger/logger.service";
 import {UsersController} from "./users/users.controller";
 import {ExeptionFilter} from "./errors/exeption.filter";
+import {Container, ContainerModule, interfaces} from "inversify";
+import {ILogger} from "./logger/logger.interface";
+import {TYPES} from "./types";
+import {IExeptionFilter} from "./errors/exeption.filter.interface";
 
-const bootstrap = async () => {
-    const logger = new LoggerService();
-    const app = new App(logger, new UsersController(logger), new ExeptionFilter(logger));
-    await app.init()
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+    bind<ILogger>(TYPES.ILogger).to(LoggerService)
+    bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter)
+    bind<UsersController>(TYPES.UserController).to(UsersController)
+    bind<App>(TYPES.Application).to(App)
+})
+
+const bootstrap = () => {
+    const appContainer = new Container();
+    appContainer.load(appBindings)
+    const app = appContainer.get<App>(TYPES.Application)
+    app.init()
+    return {app, appContainer}
 }
 
-bootstrap();
+export const {app, appContainer} = bootstrap();
+
+
