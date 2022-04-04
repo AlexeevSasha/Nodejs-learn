caeb87685fb106a24a8e3208d26e4654304b0753
AlexeevSasha <work.alexeevs@gmail.com>
2022-04-04 16:22:46 +0300

    add injectable and inject

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,15 @@
 import {BaseController} from "../common/base.controller";
-import {LoggerService} from "../logger/logger.service";
 import {NextFunction, Request, Response} from "express";
-import {HttpError} from "../errors/http-error";
+import {inject, injectable} from "inversify";
+import {TYPES} from "../types";
+import {ILogger} from "../logger/logger.interface";
+import 'reflect-metadata';
+import {IUserController} from "./users.controller.interface";
 
-export class UsersController extends BaseController {
-    constructor(logger: LoggerService) {
-        super(logger);
+@injectable()
+export class UsersController extends BaseController implements IUserController{
+    constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+        super(loggerService);
         this.bindRoutes([
             {path: '/login', method: "post", func: this.login},
             {path: '/register', method: "post", func: this.register}
