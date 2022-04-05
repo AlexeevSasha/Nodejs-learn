9be427b289fe85a54b658b3fdffcf6e24f641275
AlexeevSasha <work.alexeevs@gmail.com>
2022-04-05 13:25:21 +0300

    add bind user-service

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,35 @@
-import {App} from "./app";
-import {LoggerService} from "./logger/logger.service";
-import {UsersController} from "./users/users.controller";
-import {ExeptionFilter} from "./errors/exeption.filter";
-import {Container, ContainerModule, interfaces} from "inversify";
-import {ILogger} from "./logger/logger.interface";
-import {TYPES} from "./types";
-import {IExeptionFilter} from "./errors/exeption.filter.interface";
+import { App } from './app';
+import { LoggerService } from './logger/logger.service';
+import { UsersController } from './users/users.controller';
+import { ExeptionFilter } from './errors/exeption.filter';
+import { Container, ContainerModule, interfaces } from 'inversify';
+import { ILogger } from './logger/logger.interface';
+import { TYPES } from './types';
+import { IExeptionFilter } from './errors/exeption.filter.interface';
+import { UsersService } from "./users/users.service";
+import { IUsersService } from "./users/users.service.interface";
+import { IUserController } from "./users/users.controller.interface";
+
 
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
-    bind<ILogger>(TYPES.ILogger).to(LoggerService)
-    bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter)
-    bind<UsersController>(TYPES.UserController).to(UsersController)
-    bind<App>(TYPES.Application).to(App)
-})
+	bind<ILogger>(TYPES.ILogger).to(LoggerService);
+	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
+	bind<IUserController>(TYPES.UserController).to(UsersController);
+	bind<IUsersService>(TYPES.UsersService).to(UsersService);
+	bind<App>(TYPES.Application).to(App);
+});
 
-const bootstrap = () => {
-    const appContainer = new Container();
-    appContainer.load(appBindings)
-    const app = appContainer.get<App>(TYPES.Application)
-    app.init()
-    return {app, appContainer}
+export interface IBootstrap {
+	appContainer: Container;
+	app: App;
 }
 
-export const {app, appContainer} = bootstrap();
+const bootstrap = (): IBootstrap => {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	app.init();
+	return { app, appContainer };
+};
 
-
+export const { app, appContainer } = bootstrap();
