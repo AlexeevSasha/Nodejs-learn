0cd4163f91bf2b4581d02eb7381677268054215f
AlexeevSasha <work.alexeevs@gmail.com>
2022-04-05 13:24:57 +0300

    add user-service

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
-
 export const TYPES = {
-    Application: Symbol.for('Application'),
-    ILogger: Symbol.for('ILogger'),
-    UserController: Symbol.for('UserController'),
-    ExeptionFilter: Symbol.for('ExeptionFilter')
-}
+	Application: Symbol.for('Application'),
+	ILogger: Symbol.for('ILogger'),
+	UserController: Symbol.for('UserController'),
+	UsersService: Symbol.for('UsersService'),
+	ExeptionFilter: Symbol.for('ExeptionFilter'),
+};
