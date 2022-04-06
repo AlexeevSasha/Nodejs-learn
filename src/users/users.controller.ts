efd0f9be0a63b234526738d4ea134c9c19317873
AlexeevSasha <work.alexeevs@gmail.com>
2022-04-06 16:13:13 +0300

    add signJWT and info

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,33 +5,99 @@ import { TYPES } from '../types';
 import { ILogger } from '../logger/logger.interface';
 import 'reflect-metadata';
 import { IUserController } from './users.controller.interface';
-import { UserLoginDto } from "./dto/user-login.dto";
-import { UserRegisterDto } from "./dto/user-register.dto";
-import { UsersService } from "./users.service";
-import { HttpError } from "../errors/http-error";
-import { ValidateMiddleware } from "../common/validate.middleware";
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserRegisterDto } from './dto/user-register.dto';
+import { UsersService } from './users.service';
+import { HttpError } from '../errors/http-error';
+import { ValidateMiddleware } from '../common/validate.middleware';
+import { sign } from 'jsonwebtoken';
+import { IConfigService } from '../config/config.service.interface';
+import { AuthGuard } from "../common/auth.guard";
 
 @injectable()
 export class UsersController extends BaseController implements IUserController {
-	constructor(@inject(TYPES.ILogger) private loggerService: ILogger,
-							@inject(TYPES.UsersService) private userService : UsersService) {
+	constructor(
+		@inject(TYPES.ILogger) private loggerService: ILogger,
+		@inject(TYPES.UsersService) private userService: UsersService,
+		@inject(TYPES.ConfigService) private configService: IConfigService,
+	) {
 		super(loggerService);
 		this.bindRoutes([
 			{ path: '/login', method: 'post', func: this.login },
-			{ path: '/register', method: 'post', func: this.register, middlewares: [new ValidateMiddleware(UserRegisterDto)] },
+			{
+				path: '/register',
+				method: 'post',
+				func: this.register,
+				middlewares: [new ValidateMiddleware(UserRegisterDto)],
+			},
+			{
+				path: '/login',
+				method: 'post',
+				func: this.login,
+				middlewares: [new ValidateMiddleware(UserLoginDto)],
+			},
+			{
+				path: '/info',
+				method: 'get',
+				func: this.info,
+				middlewares: [new AuthGuard()]
+			},
 		]);
 	}
 
-	login(req: Request<{}, {}, UserLoginDto >, res: Response, next: NextFunction): void {
-		 console.log(req.body);
-		this.ok(res, 'login');
+	async login(
+		{ body }: Request<{}, {}, UserLoginDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const result = await this.userService.validateUser(body);
+		if (!result) {
+			return next(new HttpError(401, 'Неверно указанны данные'));
+		}
+		const jwt = await this.signJWT(body.email, this.configService.get('SECRET'));
+		this.ok(res, { jwt });
 	}
 
-	async register({body}: Request<{}, {}, UserRegisterDto >, res: Response, next: NextFunction): Promise<void> {
-     const result = await this.userService.createUser(body)
+	async register(
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const result = await this.userService.createUser(body);
 		if (!result) {
-			return next(new HttpError(422, 'Такой пользователь существует!'))
+			return next(new HttpError(422, 'Такой пользователь существует!'));
 		}
-		this.ok(res, {email: result.email});
+		this.ok(res, { email: result.email, id: result.id });
+	}
+
+	private signJWT(email: string, secret: string): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
+			sign(
+				{
+					email,
+					iat: Math.floor(Date.now() / 1000),
+				},
+				secret,
+				{
+					algorithm: 'HS256',
+				},
+				(err, token) => {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(token as string);
+					}
+				},
+			);
+		});
+	}
+
+	async info(
+		{ user }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const userInfo = await this.userService.getuserInfo(user)
+		this.ok(res, { email: userInfo?.email, id: userInfo?.id });
 	}
 }
