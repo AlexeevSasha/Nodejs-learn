c81de0705c41fe574fd7323115b95e488088f76e
AlexeevSasha <work.alexeevs@gmail.com>
2022-04-06 16:10:01 +0300

    add binding

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,22 @@ import { Container, ContainerModule, interfaces } from 'inversify';
 import { ILogger } from './logger/logger.interface';
 import { TYPES } from './types';
 import { IExeptionFilter } from './errors/exeption.filter.interface';
-import { UsersService } from "./users/users.service";
-import { IUsersService } from "./users/users.service.interface";
-import { IUserController } from "./users/users.controller.interface";
-
+import { UsersService } from './users/users.service';
+import { IUsersService } from './users/users.service.interface';
+import { IUserController } from './users/users.controller.interface';
+import { IConfigService } from "./config/config.service.interface";
+import { ConfigService } from "./config/config.service";
+import { PrismaService } from "./database/prisma.service";
+import { UsersRepository } from "./users/users.repository";
 
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
-	bind<ILogger>(TYPES.ILogger).to(LoggerService);
+	bind<ILogger>(TYPES.ILogger).to(LoggerService).inSingletonScope();
 	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
 	bind<IUserController>(TYPES.UserController).to(UsersController);
 	bind<IUsersService>(TYPES.UsersService).to(UsersService);
+	bind<PrismaService>(TYPES.PrismaService).to(PrismaService).inSingletonScope();
+	bind<UsersRepository>(TYPES.UsersRepository).to(UsersRepository).inSingletonScope();
+	bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
 	bind<App>(TYPES.Application).to(App);
 });
 
@@ -33,3 +39,4 @@ const bootstrap = (): IBootstrap => {
 };
 
 export const { app, appContainer } = bootstrap();
+
